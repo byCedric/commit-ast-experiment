c2e4918437795b1075d324f07b472d0d8c07d80d
Cedric van Putten <me@bycedric.com>
2019-01-23 15:17:06 +0100

    test: separate unit and integration tests and run them in travis

---
+++ test/data/integration.test.js
@@ -0,0 +1,82 @@
+import { readFileSync } from 'fs';
+import { resolve } from 'path';
+import { round } from 'lodash';
+import test from 'ava';
+import parse from 'csv-parse/lib/sync';
+import { parseString } from '../../lib/parser';
+
+const load = (type) => {
+	const file = resolve(__dirname, `commits-${type}.csv`);
+	const opts = {
+		columns: true,
+		skip_empty_lines: true
+	};
+
+	return parse(readFileSync(file, 'utf8'), opts);
+};
+
+const run = (data) => {
+	let passed = 0;
+	let failed = 0;
+
+	for (let commit of data) {
+		const body = JSON.parse(commit.body);
+
+		try {
+			// todo: parse full commit instead of header once grammar is able to
+			parseString(body.split('\n')[0])
+			passed += 1;
+		} catch (error) {
+			failed += 1;
+		}
+	}
+
+	return {
+		passed,
+		failed,
+		rate: round((passed / data.length) * 100, 1),
+	};
+};
+
+const debug = (info) => {
+	console.log(`#${info.commits} ${info.type} commits: ✓ #${info.passed} - 𐄂 #${info.failed} (${info.rate}%)`);
+};
+
+test('conventional commits data', t => {
+	const data = load('conventional');
+	const info = run(data);
+
+	debug({
+		...info,
+		type: 'conventional',
+		commits: data.length,
+	});
+
+	t.true(info.passed > info.failed);
+});
+
+test('ignored commits data', t => {
+	const data = load('ignored');
+	const info = run(data);
+
+	debug({
+		...info,
+		type: 'ignored',
+		commits: data.length,
+	});
+
+	t.true(info.passed > info.failed);
+});
+
+test('unknown commits data', t => {
+	const data = load('unknown');
+	const info = run(data);
+
+	debug({
+		...info,
+		type: 'unknown',
+		commits: data.length,
+	});
+
+	t.true(info.passed > info.failed);
+});
