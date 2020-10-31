692bdb8889e53bdfbd9390c2125dc9f14d05fe7c
Cedric van Putten <me@bycedric.com>
2020-10-31 23:37:33 +0100

    feature: write up unist v2 compatible commit ast

--- src/example.ts
+++ src/example.ts
@@ -1,5 +1,7 @@
+import { Commit } from './types';
+
 // fix(cli): this is a normal fix commit (#123)
-const example1 = {
+const example1: Commit = {
 	type: 'commit',
 	children: [
 		{
@@ -15,7 +17,7 @@ const example1 = {
 };
 
 // feat(app)!: this new feature breaks something [skip-ci]
-const example2 = {
+const example2: Commit = {
 	type: 'commit',
 	children: [
 		{
@@ -37,46 +39,67 @@ const example2 = {
 //
 // * docs: add steps for using https with docusaurus
 //
-// * improve https certs docs
+// * improve https certs docs (#987)
 //
 // * typo
 //
 // * local https: add mkcert -install step
 //
+// Fixes #123
 // Co-authored-by: slorber <lorber.sebastien@gmail.com>
-const example3 = {
+const example3: Commit = {
 	type: 'commit',
 	children: [
+		// Not sure if we should add these "raw value"-text nodes?
+		// {
+		// 	type: 'text',
+		// 	value: 'feat(v2): add support for key,cert in https (#3594)\n\n'
+		// 		+ '* fix: add support for key,cert in https\n\r\n\r'
+		// 		+ '* docs: add steps for using https with docusaurus\n\r\n\r'
+		// 		+ '* improve https certs docs (#987)\n\r\n\r'
+		// 		+ '* typo\n\r\n\r'
+		// 		+ '* local https: add mkcert -install step\n\r\n\r'
+		// 		+ 'Fixes #123\n\n'
+		// 		+ 'Co-authored-by: slorber <lorber.sebastien@gmail.com>'
+		// 	,
+		// },
 		{
 			type: 'header',
 			children: [
+				// Not sure if we should add these "raw value"-text nodes?
+				{ type: 'text', value: 'feat(v2): add support for key,cert in https (#3594)' },
 				{ type: 'type', value: 'feat' },
 				{ type: 'scope', value: 'v2' },
 				{ type: 'subject', value: 'add support for key,cert in https' },
 				{ type: 'reference', value: '3594' },
 			],
 		},
-		// TBD, one of these options:
+		{ type: 'body', children: [{ type: 'text', value: '* fix: add support for key,cert in https' }] },
+		{ type: 'body', children: [{ type: 'text', value: '* docs: add steps for using https with docusaurus' }] },
 		{
 			type: 'body',
 			children: [
-				{ type: 'text', value: '* fix: add support for key,cert in https' },
-				{ type: 'text', value: '* docs: add steps for using https with docusaurus' },
-				{ type: 'text', value: '* improve https certs docs' },
-				{ type: 'text', value: '* typo' },
-				{ type: 'text', value: '* local https: add mkcert -install step' },
+				{ type: 'text', value: '* improve https certs docs (#987)' },
+				{ type: 'reference', value: '987' },
 			],
 		},
-		// or this one
-		{ type: 'body', value: '* fix: add support for key,cert in https' },
-		{ type: 'body', value: '* docs: add steps for using https with docusaurus' },
-		{ type: 'body', value: '* improve https certs docs' },
-		{ type: 'body', value: '* typo' },
-		{ type: 'body', value: '* local https: add mkcert -install step' },
+		{ type: 'body', children: [{ type: 'text', value: '* typo' }] },
+		{ type: 'body', children: [{ type: 'text', value: '* local https: add mkcert -install step' }] },
 		{
 			type: 'footer',
 			children: [
-				{ type: 'token', value: 'Co-authored-by' },
+				// Not sure if we should add these "raw value"-text nodes?
+				{ type: 'text', value: 'Fixes #123' },
+				{ type: 'action', value: 'Fixes' },
+				{ type: 'reference', value: '123' },
+			],
+		},
+		{
+			type: 'footer',
+			children: [
+				// Not sure if we should add these "raw value"-text nodes?
+				{ type: 'text', value: 'Co-authored-by: slorber <lorber.sebastien@gmail.com>' },
+				{ type: 'action', value: 'Co-authored-by' },
 				{ type: 'text', value: 'slorber <lorber.sebastien@gmail.com>' },
 			],
 		},
--- src/types.ts
+++ src/types.ts
@@ -1,20 +1,138 @@
-export interface TokenLocation {
-	line: number;
-	column: number;
-	offset: number;
+// CAST - Commit Abstract Syntax Tree
+// based on conventional commits
+// should adhere to unist (universal syntax tree) v2.0.0
+
+import { Literal, Parent } from 'unist';
+
+/**
+ * A commit represents the root of the AST.
+ * It contains all main nodes within the commit.
+ */
+export interface Commit extends Parent {
+	type: 'commit';
+	children: (Header | Body | Footer | Text)[];
 }
 
-export interface Token<T extends string> {
-	type: T;
-	start: TokenLocation;
-	end?: TokenLocation;
+/**
+ * Commit headers refer to the conventional commits header type.
+ * It holds the most important informaiton of a single commit.
+ * May contain nodes like type, scope, subject, references, flags, and/or text.
+ */
+export interface Header extends Parent {
+	type: 'header';
+	children: (HeaderType | HeaderScope | HeaderSubject | Breaking | Reference | Flag | Text)[];
 }
 
-export interface CommitToken extends Token<'commit'> {
-
+/**
+ * Header type is a special string containing the intent of the commit.
+ * This is only defined if the commit is a conventional commit.
+ */
+export interface HeaderType extends Literal {
+	type: 'type';
+	value: string;
 }
 
-const abc: CommitToken = {
-	type: 'commit',
-	start: { line: 1, column: 0, offset: 0 },
-};
+/**
+ * Header scope is a special string containing the intent of the commit.
+ * This is only defined if the commit is a conventional commit.
+ */
+export interface HeaderScope extends Literal {
+	type: 'scope';
+	value: string;
+}
+
+/**
+ * Header subject is a special string containging the subject of the commit.
+ * This is only defined if the commit is a conventional commit.
+ *   - it MUST NOT contain optional references or flags
+ */
+export interface HeaderSubject extends Literal {
+	type: 'subject';
+	value: string;
+}
+
+/**
+ * Body contains a single line of text.
+ * It may hold textual information and/or references within that text.
+ */
+export interface Body extends Parent {
+	type: 'body';
+	children: (Text | Reference)[];
+}
+
+/**
+ * Footer contains special actions from a line of text.
+ * This can refer to actionable items with their value, like:
+ *   - Co-authored-by: author <e@mail.com>
+ *   - Fixes #123
+ */
+export interface Footer extends Parent {
+	type: 'footer',
+	children: (FooterAction | Breaking | Reference | Text)[];
+}
+
+/**
+ * Footer actions are the key from the footer's key/value pairs.
+ * It's often considered, or has been considered, actionable items.
+ */
+export interface FooterAction extends Literal {
+	type: 'action';
+	value: string;
+}
+
+/**
+ * Breaking nodes are a special type of node that may mark commits as breaking.
+ * This node contains the value with what this was makred as breaking, e.g. `!` or `BREAKING CHANGE`.
+ */
+export interface Breaking extends Literal {
+	type: 'breaking';
+	value: string;
+}
+
+/**
+ * A reference is the part of the commit that may reference external entities.
+ * This could be a pull request, commit hash, or ticket id.
+ *   - it MUST start with an hash `#` and may not contain any whitespace
+ *   - it MAY be wrapped in parenthesis `(#<reference>)`
+ */
+export interface Reference extends Literal {
+	type: 'reference';
+	value: string;
+}
+
+/**
+ * A flag is part of the commit that acts as a flag or label.
+ * This chould mark special behavior for the commit, like `skip-ci`.
+ *   - it MUST be wrapped in brackets `[<flag>]`
+ */
+export interface Flag extends Literal {
+	type: 'flag';
+	value: string;
+}
+
+/**
+ * Text is anything that's just considered text.
+ * It's used by some nodes as children, like body.
+ */
+export interface Text extends Literal {
+	type: 'text';
+	value: string;
+}
+
+/** All available types of the nodes */
+export type Types = Nodes['type'];
+/** All available node types */
+export type Nodes =
+	| Commit
+	| Header
+	| HeaderType
+	| HeaderScope
+	| HeaderSubject
+	| Body
+	| Footer
+	| FooterAction
+	| Breaking
+	| Reference
+	| Flag
+	| Text
+;
